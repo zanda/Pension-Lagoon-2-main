4885a1bf8c531dfcbd01563154089001849806e7
Zanda <yorick.mongkau@gmail.com>
2022-12-19 08:33:51 -1000

    OK, je charge "axios" pour le HACKATHON

---
+++ src/components/gestion/Gestion.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react'
+import axios from "axios";
+import { Link } from "react-router-dom";
+ 
+const ClientsList = () => {
+    const [clients, setClient] = useState([]);
+ 
+    useEffect(() => {
+        getClients();
+    }, []);
+ 
+    const getClients = async () => {
+        const response = await axios.get('http://localhost:8080/clients');
+        setClient(response.data);
+    }
+ 
+    const deleteClient = async (id) => {
+        await axios.delete(`http://localhost:8080/clients/${id}`);
+        getClients();
+    }
+ 
+    return (
+        <div>
+            <Link to="/add" className="button is-primary mt-2">Add New</Link>
+            <table className="table is-striped is-fullwidth">
+                <thead>
+                    <tr>
+                        <th>Ref</th>
+                        <th>Nom</th>
+                        <th>Prenom</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { clients.map((client: any, index) => (
+                        <tr key={ client.id }>
+                            <td>{ index + 1 }</td>
+                            <td>{ client.nom }</td>
+                            <td>{ client.prenom }</td>
+                            <td>{ client.phone }</td>
+                            <td>{ client.email }</td>
+                            <td>
+                                <Link to={`/edit/${client.id}`} className="button is-small is-info">Edit</Link>
+                                <button onClick={ () => deleteClient(client.id) } className="button is-small is-danger">Delete</button>
+                            </td>
+                        </tr>
+                    )) }
+                     
+                </tbody>
+            </table>
+        </div>
+    )
+}
+ 
+export default ClientsList
